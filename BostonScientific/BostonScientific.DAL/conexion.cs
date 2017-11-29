4179b554a318dc8c7ff3c3bfecd080babdafc660
Kevin Oleaga <oleaga@outlook.com>
2017-11-29 11:37:06 -0600

    2.0

--- BostonScientific/BostonScientific.DAL/conexion.cs
+++ BostonScientific/BostonScientific.DAL/conexion.cs
@@ -9,49 +9,5 @@ namespace BostonScientific.DAL
 {
     public class conexion
     {
-        
-        // DynamoDB Client
-        public AmazonDynamoDBClient GetClient()
-        {
-            AmazonDynamoDBClient client;
-
-            try
-            {
-                client = new AmazonDynamoDBClient(awsKey, awsSecret, awsRegion);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("\nError: Ha ocurriodo un error al obtener el cliente DynamoDB. \nDescripción: " + ex.Message + "\n");
-                return (null);
-            }
-            return (client);
-        }
-
-        // DynamoDB Context
-        public DynamoDBContext GetContext()
-        {
-            var ctx = new DynamoDBContext(GetClient());
-            return ctx;
-        }
-
-        // S3 Client
-        public AmazonS3Client S3_GetClient()
-        {
-            var awsKey = "AKIAIBK3JJO36MVM5ISQ";
-            var awsSecret = "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";
-            var awsRegion = RegionEndpoint.USWest2;
-            AmazonS3Client client;
-
-            try
-            {
-                client = new AmazonS3Client(awsKey, awsSecret, awsRegion);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("\nError: Ha ocurriodo un error al obtener el cliente de la capa S3. \nDescripción: " + ex.Message + "\n");
-                return (null);
-            }
-            return (client);
-        }
     }
 }
